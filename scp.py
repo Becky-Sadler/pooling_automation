3dcf5f78bc2b1bcdfaf27cdbcfc0ad465f584c1d
Becky Sadler <rebecca.sadler@ouh.nhs.uk>
2020-08-06 10:39:33 +0100

    Pulling variables out of the config file.

---
+++ scp.py
@@ -0,0 +1,19 @@
+import paramiko
+from scp import SCPClient
+import sys
+import yaml
+
+connection_config = yaml.safe_load(open("scp_config.yml"))
+
+scp_information = connection_config['scp_information']
+ 
+host = scp_information['Host']
+username = scp_information['Username']
+passphrase = scp_information['Passphrase']
+SSH_path = scp_information['SSH_path_to_key']
+remote_path = scp_information['remote_path']
+local_path = scp_information['local_path']
+
+with paramiko.SSHClient() as client:
+	client.set_missing_host_key_policy(paramiko.client.WarningPolicy)
+	connection = client.connect(hostname=robot_ip, username='root', passphrase='mypassphrase', key_filename='/path/to/ot2_ssh_key')
