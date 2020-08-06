ce731674ca541acb488e901b2aef397530366340
Becky Sadler <rebecca.sadler@ouh.nhs.uk>
2020-08-06 10:42:50 +0100

    Renaming scp.py to scp_upload.py to prevent error with scp library

--- scp_upload.py
+++ scp_upload.py
@@ -14,6 +14,15 @@ SSH_path = scp_information['SSH_path_to_key']
 remote_path = scp_information['remote_path']
 local_path = scp_information['local_path']
 
-with paramiko.SSHClient() as client:
+
+print(host)
+print(username)
+print(passphrase)
+print(SSH_path)
+print(remote_path)
+print(local_path)
+
+'''with paramiko.SSHClient() as client:
 	client.set_missing_host_key_policy(paramiko.client.WarningPolicy)
 	connection = client.connect(hostname=robot_ip, username='root', passphrase='mypassphrase', key_filename='/path/to/ot2_ssh_key')
+	'''
