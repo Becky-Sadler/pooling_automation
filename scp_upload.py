7c020ec4514af9b03b2c34276167dada8ce42896
Becky Sadler <rebecca.sadler@ouh.nhs.uk>
2020-08-07 10:44:00 +0100

    Completed scp script (tested using aws instance)

--- scp_upload.py
+++ scp_upload.py
@@ -2,6 +2,8 @@ import paramiko
 from scp import SCPClient
 import sys
 import yaml
+import glob
+import os
 
 connection_config = yaml.safe_load(open("scp_config.yml"))
 
@@ -12,17 +14,32 @@ username = scp_information['Username']
 passphrase = scp_information['Passphrase']
 SSH_path = scp_information['SSH_path_to_key']
 remote_path = scp_information['remote_path']
-local_path = scp_information['local_path']
+
+# Creating the SSHClient
+def connect_via_ssh(host, username, SSH_path):
+    SSHclient = paramiko.SSHClient()
+    SSHclient.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    SSHclient.connect(hostname=host, username=username, key_filename=SSH_path, port = 22)
+    return SSHclient
+
+ssh = connect_via_ssh(host, username, SSH_path)
+scp = SCPClient(ssh.get_transport())
+
+file_list = (glob.glob("*.csv"))
+
+user_input = input("Do you want to upload the following file(s): %s? (yes/no): "%(str(file_list).strip('[]')))
+	# Check the input value
+
+if user_input == 'yes':
+	print('Success')
+	for file in file_list:
+		absolute_path = os.path.abspath(file)
+		scp.put(absolute_path, remote_path)
+elif user_input == 'no':
+	print('File(s) will not be uploaded to OT-2. Please remove incorrect files from directory and run upload script again.')
+else:
+	print('Invalid input ')
 
 
-print(host)
-print(username)
-print(passphrase)
-print(SSH_path)
-print(remote_path)
-print(local_path)
 
-'''with paramiko.SSHClient() as client:
-	client.set_missing_host_key_policy(paramiko.client.WarningPolicy)
-	connection = client.connect(hostname=robot_ip, username='root', passphrase='mypassphrase', key_filename='/path/to/ot2_ssh_key')
-	'''
+
