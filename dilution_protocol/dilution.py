3af8904ba3b8f0b8e7661a7fc5aa67e496432f32
Becky Sadler <rebecca.sadler@ouh.nhs.uk>
2020-10-09 11:34:38 +0100

    Addition of offset for the tip touch - needs testing on the robot

--- dilution_protocol/dilution.py
+++ dilution_protocol/dilution.py
@@ -19,7 +19,7 @@ Custom Labware:
 Deck Layout:
 	Position 10:	OpenTrons 20ul tip rack (opentrons_96_tiprack_20ul)
 	Position 11:	OpenTrons 300ul tip rack (opentrons_96_tiprack_300ul)
-	Position 8:		NonSkirted 96-well plate in flat plate holder (Plate 1 - full strength)
+	Position 8:		NonSkirted 96-well plate in flat plate holder (Plate 1 - full strength dna)
 	Position 7:		NonSkirted 96-well plate in flat plate holder (Plate 2 - Diluted)
 	Position 4:		4x6 rack for 1.5ml microfuge tubes (Water pot - A1) ???Would water be held this way???
 
@@ -27,23 +27,21 @@ Liquid Handling properties:
 	??? Do these need to change????
 	Aspirate height: 2mm from bottom
 	Dispense height: 1mm from bottom at start increase by 0.5mm with each loop 
-	Touch tip on aspirate: 2-3mm from top
 
 Transfer process:
 	Adding water to the diluted plate:
-	Get tip 
-	Aspirate 200ul of water 
-	Touch tip (2-3mm from top)
-	Cycle through water values in csv and dispense appropriate amount in each. 
-	Have loop to make sure that the amount of water in the tip is always enough for next dispense. 
+	Use the distrbute function to draw up correct amount of water + disposal volume. 
+	Transfer water into each well + touch tip (on both the aspirate and all dispenses)
+	Blow out disposal volume and drop tip
 
+	Adding DNA to wells: 
 	Loop through the dna transfers from a csv file
 	Get tip (NEW ONE)
 	Aspirate from well at 2mm 
 	Touch tip (2-3mm from top)
 	Dispense into well (At 1mm)
-	Blow out (+0.5mm above) - ??? Should this instead be a mix ??? 
-	Touch tip 
+	Mix (aspirate and dispense 8 times with full pipette volume)
+	Touch tip again 
 	Drop tip. 
 '''
 
@@ -82,6 +80,6 @@ def run(protocol: protocol_api.ProtocolContext):
 	# Add water via distribute (disposal volume is 20ul - the minimum for the pipette)
 	right_pipette.distribute(waterlist, biomek_tube_rack['A1'], [dilution_plate.wells_by_name()[well_name] for well_name in destination_welllist], touch_tip=True, disposal_volume=20 )
 
-	# Transfer DNA using Transfer function (Mixing after each dispense with maximum volume of pipette + tip touch)
+	# Transfer DNA using Transfer function (Mixing after each dispense with maximum volume of pipette + tip touch (at -4) Need to test the offset)
 
-	left_pipette.transfer(dnalist, [dna_plate.wells_by_name()[source_well_name] for source_well_name in source_welllist], [dilution_plate.wells_by_name()[destination_well_name]for destination_well_name in destination_welllist], new_tip='always', mix_after=(8, 20), touch_tip=True)
+	left_pipette.transfer(dnalist, [dna_plate.wells_by_name()[source_well_name] for source_well_name in source_welllist], [dilution_plate.wells_by_name()[destination_well_name]for destination_well_name in destination_welllist], new_tip='always', mix_after=(8, 20), touch_tip=True, v_offset = -4.0)
